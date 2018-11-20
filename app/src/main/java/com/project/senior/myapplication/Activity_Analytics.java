87174c566fe604423e35c3eb9e2a648ff49a920d
Isaac <isaacmruiz@gmail.com>
2018-11-20 10:57:14 -0600

    Added Map to UI

---
+++ app/src/main/java/com/project/senior/myapplication/Activity_Analytics.java
@@ -0,0 +1,15 @@
+package com.project.senior.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.senior.myapplication.R;
+
+public class Activity_Analytics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analytics);
+    }
+}
