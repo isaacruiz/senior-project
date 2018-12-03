177fffeb9c215722e7ed0c4070df0e0b530a0a67
Keebz <zoopfactory@gmail.com>
2018-12-03 00:44:49 -0600

    Photo Upload Service
    Uses Firebase to store user and image data.

---
+++ PhotoUpload-master/app/src/main/java/com/takeahike/photoupload/FeedActivity.java
@@ -0,0 +1,228 @@
+package com.takeahike.photoupload;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FeedActivity extends AppCompatActivity {
+
+    FirebaseUser fbUser;
+    DatabaseReference database;
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    ImageAdapter mAdapter;
+    ArrayList<Image> images = new ArrayList<>();
+
+    static final int RC_PERMISSION_READ_EXTERNAL_STORAGE = 1;
+    static final int RC_IMAGE_GALLERY = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+
+        fbUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (fbUser == null) {
+            finish();
+        }
+
+        database = FirebaseDatabase.getInstance().getReference();
+
+        // Setup the RecyclerView
+        recyclerView = findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new ImageAdapter(images, this);
+        recyclerView.setAdapter(mAdapter);
+
+        // Get the latest 100 images
+        Query imagesQuery = database.child("images").orderByKey().limitToFirst(100);
+        imagesQuery.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                // A new image has been added, add it to the displayed list
+                final Image image = dataSnapshot.getValue(Image.class);
+
+                // get the image user
+                database.child("users/" + image.userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        image.user = user;
+                        mAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                // get image likes
+                Query likesQuery = database.child("likes").orderByChild("imageId").equalTo(image.key);
+                likesQuery.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        Like like = dataSnapshot.getValue(Like.class);
+                        image.addLike();
+                        if(like.userId.equals(fbUser.getUid())) {
+                            image.hasLiked = true;
+                            image.userLike = dataSnapshot.getKey();
+                        }
+                        mAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        Like like = dataSnapshot.getValue(Like.class);
+                        image.removeLike();
+                        if(like.userId.equals(fbUser.getUid())) {
+                            image.hasLiked = false;
+                            image.userLike = null;
+                        }
+                        mAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                mAdapter.addImage(image);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void uploadImage(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, RC_PERMISSION_READ_EXTERNAL_STORAGE);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, RC_IMAGE_GALLERY);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == RC_PERMISSION_READ_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, RC_IMAGE_GALLERY);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_IMAGE_GALLERY && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+            StorageReference imagesRef = storageRef.child("images");
+            StorageReference userRef = imagesRef.child(fbUser.getUid());
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String filename = fbUser.getUid() + "_" + timeStamp;
+            StorageReference fileRef = userRef.child(filename);
+
+            UploadTask uploadTask = fileRef.putFile(uri);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Toast.makeText(FeedActivity.this, "Upload failed!\n" + exception.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    Toast.makeText(FeedActivity.this, "Upload finished!", Toast.LENGTH_SHORT).show();
+
+                    // save image to database
+                    String key = database.child("images").push().getKey();
+                    Image image = new Image(key, fbUser.getUid(), downloadUrl.toString());
+                    database.child("images").child(key).setValue(image);
+                }
+            });
+        }
+    }
+
+    public void setLiked(Image image) {
+        if(!image.hasLiked) {
+            // add new Like
+            image.hasLiked = true;
+            Like like = new Like(image.key, fbUser.getUid());
+            String key = database.child("likes").push().getKey();
+            database.child("likes").child(key).setValue(like);
+            image.userLike = key;
+        } else {
+            // remove Like
+            image.hasLiked = false;
+            if (image.userLike != null) {
+                database.child("likes").child(image.userLike).removeValue();
+            }
+        }
+    }
+}
---
+++ PhotoUpload-master/app/src/main/java/com/takeahike/photoupload/ImageAdapter.java
@@ -0,0 +1,81 @@
+package com.takeahike.photoupload;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private ArrayList<Image> mDataset;
+    private FeedActivity mActivity;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+        public ImageView mImageView;
+        public Button mLikeButton;
+
+        public ViewHolder(View v) {
+            super(v);
+            mTextView = v.findViewById(R.id.textView2);
+            mImageView = v.findViewById(R.id.imageView);
+            mLikeButton = v.findViewById(R.id.likeButton);
+        }
+    }
+
+    public ImageAdapter(ArrayList<Image> myDataset, FeedActivity activity) {
+        mDataset = myDataset;
+        mActivity = activity;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ImageAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.image_view, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final Image image = (Image) mDataset.get(position);
+        if (image.user != null) {
+            holder.mTextView.setText(image.user.displayName);
+        }
+        Picasso.get().load(image.downloadUrl).into(holder.mImageView);
+
+        holder.mLikeButton.setText("Like (" + image.likes + ")");
+        if(image.hasLiked) {
+            holder.mLikeButton.setBackgroundColor(mActivity.getResources().getColor(R.color.colorAccent));
+        } else {
+            holder.mLikeButton.setBackgroundColor(mActivity.getResources().getColor(R.color.colorPrimary));
+        }
+        holder.mLikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mActivity.setLiked(image);
+            }
+        });
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public void addImage(Image image) {
+        mDataset.add(0, image);
+        notifyDataSetChanged();
+    }
+}
